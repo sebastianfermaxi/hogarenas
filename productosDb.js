7d9236ca8ada48237c81f97cd4192deffd0bb291
sebastianfermaxi <sebastianfermaxi@gmail.com>
2022-08-01 12:36:31 -0300

    imagenes

--- productosDb.js
+++ productosDb.js
@@ -5,42 +5,63 @@ const muzzarella = new Producto({
     productoNombre:'Muzzarella',
     productoDescripcion:'Salsa, Queso y Aceitunas',
     productoPrecio:'$ 450',
-    productoImagen:'https://cdn-icons-png.flaticon.com/512/2771/2771401.png'
+    productoImagen:'/img/muzza.png'
 });
 
 const fugazza = new Producto({
     productoNombre: 'Fugazza',
     productoDescripcion: 'Salsa, Queso, Aceitunas y Cebollas',
     productoPrecio: '$ 500',
-    productoImagen: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png'
+    productoImagen: '/img/fugazza.png'
 });
 
 const especial = new Producto({
     productoNombre: 'Especial',
     productoDescripcion: 'Salsa, Queso, Aceitunas, Jamón Cocido, Morrón y Huevo',
     productoPrecio: '$ 700',
-    productoImagen: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png'
+    productoImagen: '/img/pizzaEsp.png'
 });
 
 const empanadasCarne = new Producto({
-    productoNombre: 'Empanadas por Docena',
-    productoDescripcion: 'Con Carne Picada, Jamón y Queso',
+    productoNombre: 'Empanadas por Docena (Carne Molida)',
+    productoDescripcion: 'Con Carne Molida',
     productoPrecio: '$ 900',
-    productoImagen: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png'
+    productoImagen: '/img/empanadasCarne.png'
 });
 
-const empanadasMedia = new Producto({
-    productoNombre: 'Empanadas por Media Docena',
-    productoDescripcion: 'Con Carne Picada, Jamón y Queso',
+const empanadasCarnePicada = new Producto({
+    productoNombre: 'Empanadas por Docena (Carne Picada a Cuchillo)',
+    productoDescripcion: 'Con Carne Picada a Cuchillo',
+    productoPrecio: '$ 900',
+    productoImagen: '/img/empanadasPicada.png'
+});
+
+const empanadasCarneMedia = new Producto({
+    productoNombre: 'Empanadas por Media Docena (Carne Molida)',
+    productoDescripcion: 'Con Carne Molida',
     productoPrecio: '$ 500',
-    productoImagen: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png'
+    productoImagen: '/img/empanadasCarne.png'
+});
+
+const empanadasJQ = new Producto({
+    productoNombre: 'Empanadas por Docena (Jamón y Queso)',
+    productoDescripcion: 'Jamón y Queso',
+    productoPrecio: '$ 500',
+    productoImagen: '/img/empanadasJQ.png'
+});
+
+const empanadasJQmedia = new Producto({
+    productoNombre: 'Empanadas por Media Docena (Jamón y Queso)',
+    productoDescripcion: 'Jamón y Queso',
+    productoPrecio: '$ 500',
+    productoImagen: '/img/empanadasJQ.png'
 });
 
 const hamburguesaGrandeComp = new Producto({
     productoNombre: 'Hamburguesa Grande Completa',
     productoDescripcion: 'Tomate, Lechuga, Jamón Cocido, Huevo y Queso',
     productoPrecio: '$ 600',
-    productoImagen: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png'
+    productoImagen: '/img/hamburguesaCom.png'
 });
 
 const hamburguesaGrandeSim = new Producto({
@@ -54,7 +75,7 @@ const burguerPizza = new Producto({
     productoNombre: 'Burguer-Pizza',
     productoDescripcion: 'Tomate, Lechuga, Jamón Cocido, Huevo , Queso y Aceitunas',
     productoPrecio: '$ 1900',
-    productoImagen: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png'
+    productoImagen: '/img/burguerPizza.png'
 });
 
 const papasHogarenasMed = new Producto({
@@ -68,14 +89,14 @@ const papasHogarenasGran = new Producto({
     productoNombre: 'Papas Hogareñas Grandes',
     productoDescripcion: 'Salsa Cheddar, Peregil',
     productoPrecio: '$ 350',
-    productoImagen: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png'
+    productoImagen: '/img/papasGran.png'
 });
 
 const papasHogarenasSup = new Producto({
     productoNombre: 'Super Papas Hogareñas',
     productoDescripcion: 'Jamón Cocido, Huevo, Salsa Cheddar, Peregil',
     productoPrecio: '$ 500',
-    productoImagen: 'https://cdn-icons-png.flaticon.com/512/2771/2771401.png'
+    productoImagen: '/img/superPapasHog.png'
 });
 
 const guardarProducto = async () => {
@@ -83,7 +104,10 @@ const guardarProducto = async () => {
     await fugazza.save();
     await especial.save();
     await empanadasCarne.save();
-    await empanadasMedia.save();
+    await empanadasCarnePicada.save();
+    await empanadasCarneMedia.save();
+    await empanadasJQ.save();
+    await empanadasJQmedia.save();
     await hamburguesaGrandeComp.save();
     await hamburguesaGrandeSim.save();
     await burguerPizza.save();
