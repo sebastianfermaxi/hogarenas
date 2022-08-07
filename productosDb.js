6c02f0da5c62fff4fd2633708490ab0d1b755e23
sebastianfermaxi <sebastianfermaxi@gmail.com>
2022-08-07 14:14:00 -0300

    fonts

--- productosDb.js
+++ productosDb.js
@@ -3,91 +3,93 @@ const Producto = require('./models/Producto');
 
 const muzzarella = new Producto({
     productoNombre:'Muzzarella',
-    productoDescripcion:'Salsa, Queso y Aceitunas',
+    productoDescripcion:'Queso Muzzarella, Salsa, Aceitunas, Churry',
     productoPrecio:'$ 550',
     productoImagen:'/img/muzza.png'
 });
 
 const fugazza = new Producto({
     productoNombre: 'Fugazza',
-    productoDescripcion: 'Salsa, Queso, Aceitunas y Cebollas',
+    productoDescripcion: 'Queso Muzzarella, Salsa, Cebollas, Aceitunas, Churry',
     productoPrecio: '$ 650',
     productoImagen: '/img/fugazza.png'
 });
 
 const especial = new Producto({
     productoNombre: 'Especial',
-    productoDescripcion: 'Salsa, Queso, Aceitunas, Jamón Cocido, Morrón y Huevo',
+    productoDescripcion: 'Queso Muzzarella, Salsa, Huevo, Jamón, Morrón, Aceitunas, Churry',
     productoPrecio: '$ 850',
     productoImagen: '/img/pizzaEsp.png'
 });
 
 const empanadasCarnePicada = new Producto({
-    productoNombre: 'Empanadas por Docena (Carne Picada a Cuchillo)',
-    productoDescripcion: 'Con Carne Picada a Cuchillo',
+    productoNombre: 'Empanadas de Carne por Docena',
+    productoDescripcion: 'Carne Picada a Cuchillo, Cebolla, Especias',
     productoPrecio: '$ 1050',
     productoImagen: '/img/empanadasCarne.png'
 });
 
 const empanadasCarnePicadaMedia = new Producto({
-    productoNombre: 'Empanadas por Media Docena (Carne Picada a Cuchillo)',
-    productoDescripcion: 'Con Carne Picada a Cuchillo',
+    productoNombre: 'Empanadas de Carne por Media Docena',
+    productoDescripcion: 'Carne Picada a Cuchillo, Cebolla, Especias',
     productoPrecio: '$ 550',
     productoImagen: '/img/empanadasPicada.png'
 });
 
 const empanadasJQ = new Producto({
-    productoNombre: 'Empanadas por Docena (Jamón y Queso)',
-    productoDescripcion: 'Jamón y Queso',
+    productoNombre: 'Empanadas por Docena Jamón y Queso',
+    productoDescripcion: 'Jamón , Queso, Churry',
     productoPrecio: '$ 1050',
     productoImagen: '/img/empanadasJQ.png'
 });
 
 const empanadasJQmedia = new Producto({
-    productoNombre: 'Empanadas por Media Docena (Jamón y Queso)',
-    productoDescripcion: 'Jamón y Queso',
+    productoNombre: 'Empanadas por Media Docena Jamón y Queso',
+    productoDescripcion: 'Jamón , Queso, Churry',
     productoPrecio: '$ 550',
     productoImagen: '/img/empanadasJQ.png'
 });
 
-const hamburguesaGrandeComp = new Producto({
-    productoNombre: 'Hamburguesa Súper (Para compartir)',
-    productoDescripcion: 'Tomate, Lechuga, Jamón Cocido, Huevo y Queso',
-    productoPrecio: '$ 800',
-    productoImagen: '/img/hamburguesaComp.png'
-});
-
 const hamburguesaGrandeSim = new Producto({
     productoNombre: 'Hamburguesa Simple (Para compartir)',
-    productoDescripcion: 'Tomate, Lechuga',
+    productoDescripcion: 'Pan Saborizado con Ajo, Carne, Lechuga, Tomate, Churry y Aderezos',
     productoPrecio: '$ 600',
     productoImagen: '/img/hamburguesaSimp.png'
 });
 
+const hamburguesaGrandeComp = new Producto({
+    productoNombre: 'Hamburguesa Súper (Para compartir)',
+    productoDescripcion: 'Pan Saborizado con Ajo, Carne, Jamón, Queso, Huevo, Lechuga, Tomate, Churry y Aderezos',
+    productoPrecio: '$ 800',
+    productoImagen: '/img/hamburguesaComp.png'
+});
+
+
+
 const burguerPizza = new Producto({
     productoNombre: 'Burguer-Pizza',
-    productoDescripcion: 'Tomate, Lechuga, Jamón Cocido, Huevo , Queso y Aceitunas',
+    productoDescripcion: 'Queso Muzzarella, Salsa, Aceitunas, Carne, Jamón, Queso, Huevo, Lechuga, Tomate, Churry y Aderezos',
     productoPrecio: '$ 2200',
     productoImagen: '/img/burguerPizza.png'
 });
 
 const papasHogarenasMed = new Producto({
     productoNombre: 'Papas Hogareñas Medianas',
-    productoDescripcion: 'Salsa Cheddar, Peregil',
+    productoDescripcion: 'Papas al Horno, Especias, Salsa Cheddar, Peregil',
     productoPrecio: '$ 350',
     productoImagen: '/img/papasMed.png'
 });
 
 const papasHogarenasGran = new Producto({
     productoNombre: 'Papas Hogareñas Grandes',
-    productoDescripcion: 'Salsa Cheddar, Peregil',
+    productoDescripcion: 'Papas al Horno, Especias, Salsa Cheddar, Peregil',
     productoPrecio: '$ 400',
     productoImagen: '/img/papasGran.png'
 });
 
 const papasHogarenasSup = new Producto({
     productoNombre: 'Super Papas Hogareñas',
-    productoDescripcion: 'Jamón Cocido, Huevo, Salsa Cheddar, Peregil',
+    productoDescripcion: 'Papas al Horno, Especias, Jamón, Huevo, Salsa Cheddar, Peregil',
     productoPrecio: '$ 550',
     productoImagen: '/img/papasHogar.png'
 });
@@ -100,8 +102,8 @@ const guardarProducto = async () => {
     await empanadasCarnePicadaMedia.save();
     await empanadasJQ.save();
     await empanadasJQmedia.save();
-    await hamburguesaGrandeComp.save();
     await hamburguesaGrandeSim.save();
+    await hamburguesaGrandeComp.save();
     await burguerPizza.save();
     await papasHogarenasMed.save();
     await papasHogarenasGran.save();
